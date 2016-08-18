14286ab4c96132748c82057325ad651ff1ce6249
Joas Schilling <coding@schilljs.com>
2016-08-18 11:22:30 +0200

    Move to a new section

--- lib/Settings/Admin.php
+++ lib/Settings/Admin.php
@@ -67,7 +67,7 @@ class Admin implements ISettings {
 	 * @return string the section ID, e.g. 'sharing'
 	 */
 	public function getSection() {
-		return 'additional';
+		return 'files_accesscontrol';
 	}
 
 	/**
