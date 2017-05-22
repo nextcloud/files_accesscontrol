ccb81a840d49a18397ba35b2168b65b51503fe73
Joas Schilling <coding@schilljs.com>
2017-05-22 13:55:45 +0200

    Fix language error
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- l10n/zh_CN.js
+++ l10n/zh_CN.js
@@ -4,6 +4,6 @@ OC.L10N.register(
     "No rule given" : "没有定义规则",
     "File access control" : "文件访问控制",
     "Restrict access to files based on factors such as filetype, user group memberships, time and more." : "根据文件类型，用户组成员资格，时间等因素限制对文件的访问。",
-    "Each rule group consists of one or more rules. A request matches a group if all rules evaluate to true. If a request matches at least one of the defined groups, the request is blocked and the file content can not be read or written." : "每个规则组由一个或多个规则组成。 如果所有规则都匹配，则该请求匹配整个组。 如果请求与定义的组中至少一个匹配，则该请求被阻止，并且将无法读取或写入文件内容。"
+    "Each rule group consists of one or more rules. A request matches a group if all rules evaluate to true. If a request matches at least one of the defined groups, the request is blocked and the file content can not be read or written." : "每个规则组由一个或多个规则组成。 如果所有规则都匹配，则该请求匹配整个组。 如果请求与定义的组中至少一个匹配，则该请求被阻止，并且将无法读取或写入文件内容。"
 },
 "nplurals=1; plural=0;");
