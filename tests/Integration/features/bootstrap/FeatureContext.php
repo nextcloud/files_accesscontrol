379f6dfb982f6eedb7392e63c5cf6c06dbe86890
Joas Schilling <coding@schilljs.com>
2026-03-02 17:12:33 +0100

    chore(behat): Migrate to attributes
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/Integration/features/bootstrap/FeatureContext.php
+++ tests/Integration/features/bootstrap/FeatureContext.php
@@ -13,7 +13,10 @@ require __DIR__ . '/WebDav.php';
 
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\TableNode;
+use Behat\Hook\AfterScenario;
+use Behat\Hook\BeforeScenario;
 use Behat\Step\Given;
+use Behat\Step\Then;
 use Behat\Step\When;
 use GuzzleHttp\Client;
 use GuzzleHttp\Cookie\CookieJar;
@@ -60,10 +63,8 @@ class FeatureContext implements Context {
 		$this->baseUrl = getenv('TEST_SERVER_URL');
 	}
 
-	/**
-	 * @BeforeScenario
-	 * @AfterScenario
-	 */
+	#[BeforeScenario]
+	#[AfterScenario]
 	public function cleanUpBetweenTests() {
 		$this->setCurrentUser('admin');
 		$this->sendingTo('DELETE', '/apps/files_accesscontrol_testing');
@@ -76,9 +77,7 @@ class FeatureContext implements Context {
 		}
 	}
 
-	/**
-	 * @AfterScenario
-	 */
+	#[AfterScenario]
 	public function tearDown() {
 		foreach ($this->createdUsers as $user) {
 			$this->deleteUser($user);
@@ -92,9 +91,7 @@ class FeatureContext implements Context {
 		}
 	}
 
-	/**
-	 * @Given /^Ensure tag exists$/
-	 */
+	#[Given(pattern: 'Ensure tag exists')]
 	public function createTag() {
 		$this->setCurrentUser('admin');
 		$this->sendingTo('POST', '/apps/files_accesscontrol_testing');
@@ -105,9 +102,7 @@ class FeatureContext implements Context {
 		$this->tagId = $data['tagId'];
 	}
 
-	/**
-	 * @Given /^user "([^"]*)" tags file "([^"]*)"$/
-	 */
+	#[Given(pattern: '/^user "([^"]*)" tags file "([^"]*)"$/')]
 	public function tagFile(string $user, string $path) {
 		// TODO: Remove all created tags?
 		$this->setCurrentUser($user);
@@ -117,9 +112,7 @@ class FeatureContext implements Context {
 		$this->assertStatusCode($this->response, 200);
 	}
 
-	/**
-	 * @Given /^user "([^"]*)" creates (global|user) flow with (\d+)$/
-	 */
+	#[Given(pattern: '/^user "([^"]*)" creates (global|user) flow with (\d+)$/')]
 	public function createFlow(string $user, string $scope, int $statusCode, TableNode $tableNode) {
 		$this->setCurrentUser($user);
 
@@ -140,9 +133,7 @@ class FeatureContext implements Context {
 		Assert::assertSame($statusCode, $this->response->getStatusCode(), 'HTTP status code mismatch:' . "\n" . $this->response->getBody()->getContents());
 	}
 
-	/**
-	 * @Given /^user "([^"]*)" shares file "([^"]*)" with user "([^"]*)"$/
-	 */
+	#[Given(pattern: '/^user "([^"]*)" shares file "([^"]*)" with user "([^"]*)"$/')]
 	public function userSharesFile(string $sharer, string $file, string $sharee): void {
 		$this->setCurrentUser($sharer);
 		$this->sendingToWith('POST', '/apps/files_sharing/api/v1/shares', [
@@ -153,9 +144,7 @@ class FeatureContext implements Context {
 		]);
 	}
 
-	/**
-	 * @Given /^user "([^"]*)" shares file "([^"]*)" publicly$/
-	 */
+	#[Given(pattern: '/^user "([^"]*)" shares file "([^"]*)" publicly$/')]
 	public function userSharesFilePublicly(string $sharer, string $file): void {
 		$this->setCurrentUser($sharer);
 		$this->sendingToWith('POST', '/apps/files_sharing/api/v1/shares', [
@@ -178,12 +167,8 @@ class FeatureContext implements Context {
 	}
 
 	// ChecksumsContext
-	/**
-	 * @Then The webdav response should have a status code :statusCode
-	 * @param string $statusCode
-	 * @throws \Exception
-	 */
-	public function theWebdavResponseShouldHaveAStatusCode($statusCode) {
+	#[Then(pattern: 'The webdav response should have a status code :statusCode')]
+	public function theWebdavResponseShouldHaveAStatusCode(string $statusCode) {
 		if (str_contains($statusCode, '|')) {
 			$statusCodes = array_map('intval', explode('|', $statusCode));
 		} else {
@@ -195,7 +180,7 @@ class FeatureContext implements Context {
 	}
 
 
-	#[\Behat\Step\Given('the following :appId app config is set')]
+	#[Given('the following :appId app config is set')]
 	public function setAppConfig(string $appId, TableNode $formData): void {
 		$this->setCurrentUser('admin');
 		foreach ($formData->getRows() as $row) {
@@ -209,21 +194,14 @@ class FeatureContext implements Context {
 	/**
 	 * User management
 	 */
-
-	/**
-	 * @Given /^as user "([^"]*)"$/
-	 * @param string $user
-	 */
+	#[Given('/^as user "([^"]*)"$/')]
 	public function setCurrentUser(string $user): string {
 		$before = $this->currentUser;
 		$this->currentUser = $user;
 		return $before ?? 'admin';
 	}
 
-	/**
-	 * @Given /^user "([^"]*)" exists$/
-	 * @param string $user
-	 */
+	#[Given('/^user "([^"]*)" exists$/')]
 	public function assureUserExists(string $user): void {
 		try {
 			$this->userExists($user);
@@ -343,22 +321,12 @@ class FeatureContext implements Context {
 	 * Requests
 	 */
 
-	/**
-	 * @When /^sending "([^"]*)" to "([^"]*)"$/
-	 * @param string $verb
-	 * @param string $url
-	 */
+	#[When(pattern: '/^sending "([^"]*)" to "([^"]*)"$/')]
 	public function sendingTo(string $verb, string $url): void {
-		$this->sendingToWith($verb, $url, null);
+		$this->sendingToWith($verb, $url);
 	}
 
-	/**
-	 * @When /^sending "([^"]*)" to "([^"]*)" with$/
-	 * @param string $verb
-	 * @param string $url
-	 * @param array|null $body
-	 * @param array $headers
-	 */
+	#[When(pattern: '/^sending "([^"]*)" to "([^"]*)" with$/')]
 	public function sendingToWith(string $verb, string $url, ?array $body = null, array $headers = []): void {
 		$fullUrl = $this->baseUrl . 'ocs/v2.php' . $url;
 		$client = new Client();
@@ -388,10 +356,6 @@ class FeatureContext implements Context {
 		}
 	}
 
-	/**
-	 * @param ResponseInterface $response
-	 * @param int $statusCode
-	 */
 	protected function assertStatusCode(ResponseInterface $response, int $statusCode): void {
 		Assert::assertEquals($statusCode, $response->getStatusCode());
 	}
